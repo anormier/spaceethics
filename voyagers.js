ae90a10e724e6489668a7707bf11dfc5af3f9533
anormier <119351925+anormier@users.noreply.github.com>
2023-08-24 23:09:18 +0100

    Update voyagers.js

---
+++ voyagers.js
@@ -0,0 +1,11 @@
+const allVoyagers = 
+
+[
+    { id: "Voyager I", dist2024: 155, ra: 258, dec: 12, dateSent: "1977-09-05", speed: 17 },
+    { id: "Voyager II", dist2024: 130, ra: 291, dec: -56, dateSent: "1977-08-20", speed: 15 },
+    { id: "New Horizon", dist2024: 54, ra: 285, dec: -20, dateSent: "1977-08-20", speed: 14 },
+    { id: "Pioneer 11", dist2024: 100, ra: 282.25, dec: -8, dateSent: "1973-04-06", speed: 11 },
+    { id: "Pioneer 10", dist2024: 123, ra: 37.5, dec: 3, dateSent: "1972-03-03", speed: 12 }
+]
+
+export default allVoyagers;
