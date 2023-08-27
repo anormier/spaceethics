d788310b49dfd4e6efd71de0cd28cb02beed3005
anormier <119351925+anormier@users.noreply.github.com>
2023-08-27 19:34:52 +0200

    Add files via upload

--- utils.js
+++ utils.js
@@ -24,3 +24,34 @@ export function isDesktop() {
   // If there are no touch events and the screen width is above the threshold, it's probably a desktop
   return !hasTouch && window.innerWidth >= minWidth;
 }
+
+export function toggleFullScreen() {
+  if (!document.fullscreenElement && 
+      !document.webkitFullscreenElement && 
+      !document.mozFullScreenElement && 
+      !document.msFullscreenElement) {
+
+      // If not in fullscreen mode, enter it
+      if (document.documentElement.requestFullscreen) {
+          document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+          document.documentElement.webkitRequestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) {
+          document.documentElement.msRequestFullscreen();
+      }
+  } else {
+      // If in fullscreen mode, exit it
+      if (document.exitFullscreen) {
+          document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+      }
+  }
+}
+document.getElementById('fullscreen-btn').addEventListener('click', toggleFullScreen);
