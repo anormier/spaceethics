60c8f62a7bd4da14b5f83b7194f0c0f0b0a7b054
Adrien Normier <anormier@gmail.com>
2023-09-18 17:25:30 +0200

    updated voyagers

--- src/js/data/voyagers.js
+++ src/js/data/voyagers.js
@@ -1,11 +1,76 @@
-const allVoyagers = 
 
-[
-    { id: "Voyager I", r: 155, ra: 258, dec: 12, vra:0,vdec:0, dateSent: "1977-09-05", vr: 17, epoch: "2024-01-01" },
-    { id: "Voyager II", r: 130, ra: 291, dec: -56,vra:0,vdec:0, dateSent: "1977-08-20", vr: 15 , epoch: "2024-01-01"},
-    { id: "New Horizon", r: 54, ra: 285, dec: -20, vra:0,vdec:0,dateSent: "1977-08-20", vr: 14, epoch: "2024-01-01" },
-    { id: "Pioneer 11", r: 100, ra: 282.25, dec: -8,vra:0,vdec:0, dateSent: "1973-04-06", vr: 11 , epoch: "2024-01-01"},
-    { id: "Pioneer 10", r: 123, ra: 37.5, dec: 3, vra:0,vdec:0,dateSent: "1972-03-03", vr: 12, epoch: "2024-01-01" }
-]
+const allVoyagers = [
+    {
+        id: "Voyager I",
+        r: 155,
+        ra: 258,
+        dec: 12,
+        vra: 0,
+        vdec: 0,
+        dateSent: "1977-09-05",
+        vr: 17,
+        epoch: "2024-01-01",
+        nameSet: "Voyager I",
+        textSet: "Launched by NASA in 1977, Voyager 1's primary mission was to study the outer Solar System. It became the first spacecraft to enter interstellar space. It carries the Golden Record, a time capsule containing sounds and images to portray life on Earth.",
+        refURL: "https://en.wikipedia.org/wiki/Voyager_1"
+    },
+    {
+        id: "Voyager II",
+        r: 130,
+        ra: 291,
+        dec: -56,
+        vra: 0,
+        vdec: 0,
+        dateSent: "1977-08-20",
+        vr: 15,
+        epoch: "2024-01-01",
+        nameSet: "Voyager II",
+        textSet: "Voyager 2, twin to Voyager 1, is the only spacecraft to have visited both ice giants, Uranus and Neptune. It too carries a copy of the Golden Record and continues to send back data from interstellar space.",
+        refURL: "https://en.wikipedia.org/wiki/Voyager_2"
+    },
+    {
+        id: "New Horizon",
+        r: 54,
+        ra: 285,
+        dec: -20,
+        vra: 0,
+        vdec: 0,
+        dateSent: "1977-08-20",
+        vr: 14,
+        epoch: "2024-01-01",
+        nameSet: "New Horizon",
+        textSet: "New Horizons was launched in 2006 with a primary mission to perform a flyby study of the Pluto system, and later, the Kuiper belt object, Arrokoth. It provided the most detailed images and data about Pluto.",
+        refURL: "https://en.wikipedia.org/wiki/New_Horizons"
+    },
+    {
+        id: "Pioneer 11",
+        r: 100,
+        ra: 282.25,
+        dec: -8,
+        vra: 0,
+        vdec: 0,
+        dateSent: "1973-04-06",
+        vr: 11,
+        epoch: "2024-01-01",
+        nameSet: "Pioneer 11",
+        textSet: "Pioneer 11, launched in 1973, became the first spacecraft to visit Saturn. It provided detailed images of the planet and its moons. Additionally, it studied the asteroid belt and the environment around Jupiter.",
+        refURL: "https://en.wikipedia.org/wiki/Pioneer_11"
+    },
+    {
+        id: "Pioneer 10",
+        r: 123,
+        ra: 37.5,
+        dec: 3,
+        vra: 0,
+        vdec: 0,
+        dateSent: "1972-03-03",
+        vr: 12,
+        epoch: "2024-01-01",
+        nameSet: "Pioneer 10",
+        textSet: "Pioneer 10 was the first spacecraft to travel through the asteroid belt and make a flyby of Jupiter. It provided the first close-up images of the planet and was the first human-made object to achieve escape velocity from the Solar System.",
+        refURL: "https://en.wikipedia.org/wiki/Pioneer_10"
+    }
+];
+
 
 export default allVoyagers;
